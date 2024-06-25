9654b422a4fc851b72ba8a2ccc020ee6a04035c6
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2024-06-26 00:23:45 +0200

    Move menu script

--- public/src/index.js
+++ public/src/index.js
@@ -1,31 +1,3 @@
 const bases = new Bases();
 const map = new Map();
 
-
-const menuVisibilityOptions = document.getElementsByName('menu-visibility-option');
-
-
-const handleVisibilityOptionsChange = (e) => {
-    if (e.target.checked)
-    {
-        map.showBases();
-    }
-    else
-    {
-        map.hideBases();
-    }
-}
-
-
-menuVisibilityOptions.forEach(menuVisibilityOption => {
-    switch (menuVisibilityOption.value) {
-
-        case "base":
-            menuVisibilityOption.addEventListener('change', e => handleVisibilityOptionsChange(e));
-            break;
-    
-        default:
-            break;
-    }    
-});
-
--- public/src/menu.js
+++ public/src/menu.js
@@ -1,7 +1,43 @@
 const menuButton = document.getElementById('menuButton');
 const menu = document.getElementById('menu');
 
+const menuVisibilityOptions = document.getElementsByName('menu-visibility-option');
+
+
+const handleBaseVisibility = (e) => {
+    if (e.target.checked) {
+        map.showBases();
+    } else {
+        map.hideBases();
+    }
+}
+
+// const handleStatusVisibility = (e) => {
+//     if (e.target.checked) {
+//         map.showStatusDescription();
+//     } else {
+//         map.hideStatusDescription();
+//     }
+// }
+
+
 menuButton.addEventListener('click', function() {
     menuButton.classList.toggle('open');
     menu.classList.toggle('open');
+});
+
+menuVisibilityOptions.forEach(menuVisibilityOption => {
+    switch (menuVisibilityOption.value) {
+
+        case "base":
+            menuVisibilityOption.addEventListener('change', e => handleBaseVisibility(e));
+            break;
+
+        // case "status":
+        //     menuVisibilityOption.addEventListener('change', e => handleStatusVisibility(e));
+        //     break;
+    
+        default:
+            break;
+    }    
 });
