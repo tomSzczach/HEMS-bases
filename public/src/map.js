5a040c1a8dc23059aae4e8bfba79f1a3ed7a1b99
Tomasz Szcząchor <89033748+tomSzczach@users.noreply.github.com>
2025-03-16 00:37:25 +0100

    Change initial map zoom

--- public/src/map.js
+++ public/src/map.js
@@ -4,7 +4,7 @@ class Map {
 
 
     constructor() {
-        this.#map = L.map('map').setView([52, 19], 6);
+        this.#map = L.map('map').setView([52.2, 19], 7);
 
         L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
             attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
